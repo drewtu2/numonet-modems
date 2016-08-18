8d0acf9930ae03b93b9db6ee51ae2415cc438873
Andrew Tu <drewtu2@yahoo.com>
2016-08-18 09:03:56 -0400

    Updated main to use modem objects direclty instead of as pointers -> learning how to instantiate without "new" keyword

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main() {
 
-  Modem *Yashar = new Modem::Modem(1, 2);
-  Modem *Andrew = new Modem::Modem(3, 4);
-  
+  Modem Yashar(1);
+  Modem Andrew(3);
+
   int input;
 
   while (input != -1) {
-    cout << "Andrew's Local Address: " << Andrew->GetLocalAddress() << endl;
-    cout << "Yashar's Local Address: " << Yashar->GetLocalAddress() << endl;
+    cout << "Andrew's Local Address: " << Andrew.get_local_address() << endl;
+    cout << "Yashar's Local Address: " << Yashar.get_local_address() << endl;
     
     cout << "Enter new local address for Andrew: ";
     cin >> input;
-    Andrew->SetLocalAddress(input);
+    Andrew.set_local_address(input);
   }
   
 
